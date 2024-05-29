3a96d71c2c8f26265aba8a39a93b187ec0b25e59
Miguel Ozana <107505846+miguel-ozana@users.noreply.github.com>
2024-05-28 22:33:55 -0300

    done

--- src/app/components/contacts-section.tsx
+++ src/app/components/contacts-section.tsx
@@ -54,8 +54,8 @@ const ContactsSection = () => {
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
+          currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, i'll
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
