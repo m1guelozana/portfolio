dc2bf5b6f316ab8d3b16529272f65666a6d9c344
Miguel Ozana <107505846+miguel-ozana@users.noreply.github.com>
2024-05-28 21:17:14 -0300

    done

--- src/app/api/send-mail.ts
+++ src/app/api/send-mail.ts
@@ -8,6 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     try {
       const { email, subject, message } = req.body;
+
       if (!fromEmail) {
         throw new Error("FROM_EMAIL environment variable is not set");
       }
