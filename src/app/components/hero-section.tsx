220302646e9d4b0fdcba12a904793fc03d62364b
Miguel Ozana <107505846+miguel-ozana@users.noreply.github.com>
2024-05-28 23:27:40 -0300

    done

--- src/app/components/hero-section.tsx
+++ src/app/components/hero-section.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 
+
+
 const HeroSection = () => {
   return (
     <section>
@@ -35,10 +37,10 @@ const HeroSection = () => {
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#dd3e54] to-[#6be585] hover:bg-[#cc0000] text-white">
-              Hire me
+              <a href="#contact">Hire Me</a>
             </button>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent text-white hover:bg-[#6be585] hover:text-black border border-white hover:border-black mt-3">
-              Download CV
+              <a href="https://www.canva.com/design/DAGELtEUQ0g/k2CXXxQgmEj0rJuY-93M1A/view?utm_content=DAGELtEUQ0g&utm_campaign=designshare&utm_medium=link&utm_source=editor">Download CV</a>
             </button>
           </div>
         </div>
