aeee59c862897fde8b40759d2ea0020146723cfe
Miguel Ozana <107505846+miguel-ozana@users.noreply.github.com>
2024-05-28 21:08:43 -0300

    done portfolio

--- frontend/src/app/components/contacts-section.tsx
+++ frontend/src/app/components/contacts-section.tsx
@@ -57,10 +57,10 @@ const ContactsSection = () => {
             try my best to get back to you!
           </p>
           <div className="socials flex flex-row gap-2">
-            <Link href="github.com">
+            <Link href="https://github.com/miguel-ozana">
               <Image src={GithubIcon} alt="Github Icon" />
             </Link>
-            <Link href="linkedin.com">
+            <Link href="https://www.linkedin.com/in/miguel-ozana-951855231/">
               <Image src={LinkedinIcon} alt="Linkedin Icon" />
             </Link>
           </div>
