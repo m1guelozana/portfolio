585f4f04267b6b57adf2aa28b75f02b97d5b62a6
Miguel Ozana <107505846+miguel-ozana@users.noreply.github.com>
2024-08-27 19:16:34 -0300

    Update about-section.tsx

--- src/app/components/about-section.tsx
+++ src/app/components/about-section.tsx
@@ -87,10 +87,7 @@ const AboutSection: React.FC = () => {
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
             I&apos;m a Full stack developer with a passion for creating interactive
-            and responsive web applications. I have experience with JavaScript,
-            TypeScript, React, Node.Js, Express, Python, Django, Flask,
-            Selenium, Pandas, PostgreSQL, HTML, CSS, Git, Github, and NoSQL
-            Databases. I am a quick learner and I am always looking to expand my
+            and responsive web applications. I am a quick learner and I am always looking to expand my
             knowledge and skill set. I am a team player and I am excited to work
             with others to create amazing applications.
           </p>
